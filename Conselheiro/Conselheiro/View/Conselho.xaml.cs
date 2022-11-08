0c6a42b39ad9ace542692dbb497b24387f341b59
Usuario <Usuario@CPD02-M07>
2022-11-08 12:27:13 -0300

    incio da programação da view

---
+++ Conselheiro/Conselheiro/View/Conselho.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Conselheiro.Model;
+using Conselheiro.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace Conselheiro.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Conselho : ContentPage
+    {
+        public Conselho()
+        {
+            InitializeComponent();
+        }
+    }
+}
